b61ecf93a8669bdf320ce938f9ec97ca8d22ce2b
Irene Canuti <irene.canuti@gmail.com>
2017-02-06 19:40:27 +0000

    Add view with form to create a new listing. Add GET and POST requests in the controller.

--- app/app.rb
+++ app/app.rb
@@ -4,10 +4,19 @@ class MakersBnB < Sinatra::Base
 
   register Sinatra::Flash
   enable :sessions
-  
+
   get '/' do
     'Hello MakersBnB!'
   end
+
+  get '/listings/new' do
+    erb :'listings/new'
+  end
+
+  post '/listings' do
+    
+    redirect '/'
+    end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
--- spec/features/new_listing_spec.rb
+++ spec/features/new_listing_spec.rb
@@ -3,6 +3,21 @@
 # So that potential guests can see my property;
 # I want to be able to list it on MakerBnB and see it listed.
 
+# User Story 4.1
+# As a User;
+# So that I can better market my property;
+# I want to be able to name it as I want.
+
+# User Story 4.2
+# As a User;
+# So that I can give guests more information;
+# I want to be able to include a short description.
+
+# User Story 4.3
+# As a User;
+# So that the customer knows the price;
+# I want to be able to include a price per night.
+
 feature 'Create New Listing' do
   scenario 'A User wants to list Space' do
     visit '/listings/new'
