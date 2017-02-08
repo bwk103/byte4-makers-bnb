e584a2a7ee971f55b5ba995435ca3ad0523d4290
Samir Gossain <samir.gossain@gmail.com>
2017-02-08 16:45:00 +0000

    Fixed failing tests after merge

--- spec/features/booking_spec.rb
+++ spec/features/booking_spec.rb
@@ -8,6 +8,7 @@ feature 'Booking' do
 
   scenario 'user can see the availability on the booking page' do
     create_listing
+    log_out_and_login_with_another_user
     click_link "Book"
     expect(current_path).to have_content '/spaces/'
     expect(page).to have_content 'Available from: 04/07/2017 to: 08/07/2017'
@@ -15,14 +16,16 @@ feature 'Booking' do
 
   scenario 'user can book on an available date' do
     create_listing
+    log_out_and_login_with_another_user
     click_link "Book"
     fill_in(:date, with: '2017-07-05')
     click_button 'Request to Book'
-    expect(page).to have_content 'Your booking request has been submitted!'
+    expect(page).to have_content 'Requests you have made:'
   end
 
   scenario 'user can\'t book on an unavailable date' do
     create_listing
+    log_out_and_login_with_another_user
     click_link "Book"
     fill_in(:date, with: '2016-07-05')
     click_button 'Request to Book'
