d16fe268c4d333efa32433b9fd085fbbd745d6ca
Samir Gossain <samir.gossain@gmail.com>
2017-02-08 15:01:28 +0000

    Added Feature; only available dates can be booked - Tests OK

--- spec/features/booking_spec.rb
+++ spec/features/booking_spec.rb
@@ -5,14 +5,6 @@
 # I want to be able to send a request to the Host.
 
 feature 'Booking' do
-  scenario 'user submit request for a listed space' do
-    create_listing
-    click_link "Book"
-    expect(current_path).to have_content '/spaces/'
-    click_button 'Request to Book'
-    expect(current_path).to eq '/requests'
-    expect(page).to have_content 'Your booking request has been submitted!'
-  end
 
   scenario 'user can see the availability on the booking page' do
     create_listing
@@ -20,4 +12,21 @@ feature 'Booking' do
     expect(current_path).to have_content '/spaces/'
     expect(page).to have_content 'Available from: 04/07/2017 to: 08/07/2017'
   end
+
+  scenario 'user can book on an available date' do
+    create_listing
+    click_link "Book"
+    fill_in(:date, with: '2017-07-05')
+    click_button 'Request to Book'
+    expect(page).to have_content 'Your booking request has been submitted!'
+  end
+
+  scenario 'user can\'t book on an unavailable date' do
+    create_listing
+    click_link "Book"
+    fill_in(:date, with: '2016-07-05')
+    click_button 'Request to Book'
+    expect(page).to have_content 'Unavailable Date.'
+  end
+
 end
