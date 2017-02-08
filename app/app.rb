347d85d3fb45c8a1668f290a30a4fcf052d43ebb
Ben Kielty <bwk103@gmail.com>
2017-02-08 12:10:20 +0000

    WIP preventing users from booking when dates unavailable

--- app/app.rb
+++ app/app.rb
@@ -57,8 +57,21 @@ class MakersBnB < Sinatra::Base
   end
 
   post '/requests' do
-    @booking = Booking.create(guest_id: session[:user_id], request_text: params[:text], status: 'Not confirmed', space_id: session[:space_id])
+    @booking = Booking.new(guest_id: session[:user_id], request_text: params[:text], status: 'Not confirmed', space_id: session[:space_id])
     @space = Space.get(@booking.space_id)
+    available_dates = (@space.start_date..@space.end_date)
+      available_dates.each do |date|
+        p date
+      end
+      p params[:date]
+
+    if available_dates.include?(params[:date])
+      p "This printed"
+      # @booking.save
+    else
+      p "Not available"
+      # redirect '/requests'
+    end
     erb :requests
   end
 
