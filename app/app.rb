e9ff14c8d4a3d2c4c0b6975924251d96975f6acc
Ekaterina Loshchinina <KateLoschinina@Ekaterinas-MacBook-Pro-3.local>
2017-02-09 14:58:27 +0000

    Tech commit.

--- app/app.rb
+++ app/app.rb
@@ -32,8 +32,7 @@ class MakersBnB < Sinatra::Base
 
   post '/spaces' do
     space = Space.create(title: params[:title], description: params[:description],price: params[:price], user_id: session[:user_id], start_date: params[:start_date], end_date: params[:end_date])
-    @filename = params[:myfile][:filename]
-    p @filename
+    session[:filename] = params[:myfile][:filename]
     Tag.create(name: params[:pet_friendly], space_id: space.id )
     Tag.create(name: params[:house], space_id: space.id )
     Tag.create(name: params[:has_a_pool], space_id: space.id )
@@ -53,7 +52,7 @@ class MakersBnB < Sinatra::Base
   get '/spaces' do
     @spaces = Space.all
     @tags = Tag.all
-    @picture = Picture.first
+    @filename = session[:filename]
     erb :'spaces/index'
   end
 
