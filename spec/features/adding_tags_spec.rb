3e1a4fb6bf91ca517af6215f7f80b72a4c4c334f
Samir Gossain <samir.gossain@gmail.com>
2017-02-09 10:59:33 +0000

    Initial Branch Commit

---
+++ spec/features/adding_tags_spec.rb
@@ -0,0 +1,14 @@
+# User Story 4.6
+# As a User;
+# So that guests can filter my property by features;
+# I want to be able to choose tags to my property.
+
+feature 'Adding tags' do
+
+  scenario 'I can add a single tag to a property' do
+    create_listing
+    expect(page).to have_content('Pet-friendly')
+    expect(page).to have_content('House')
+  end
+
+end
