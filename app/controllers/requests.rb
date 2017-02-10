e7bbb70dc080f7cffd5da4ea4c7445daacc23da0
Ben Kielty <bwk103@gmail.com>
2017-02-10 11:57:33 +0000

    Refactoring reqests model / controller'

--- app/controllers/requests.rb
+++ app/controllers/requests.rb
@@ -3,9 +3,7 @@ class MakersBnB < Sinatra::Base
   post '/requests' do
     @booking = Booking.new(guest_id: session[:user_id], request_text: params[:text], status: 'Not confirmed', space_id: session[:space_id], date: Date.strptime(params[:date], '%Y-%m-%d'))
     @space = Space.get(@booking.space_id)
-    available_dates = (@space.start_date..@space.end_date)
-    unavailable_dates = Booking.all(:space_id => @booking.space_id, :status => "Confirmed").map {|booking| booking.date}
-    if (available_dates.include? @booking.date) && !(unavailable_dates.include? @booking.date)
+    if @space.is_available_on(@booking.date)
       @booking.save
       redirect 'users/requests'
     else
--- app/models/space.rb
+++ app/models/space.rb
@@ -1,5 +1,6 @@
 require 'data_mapper'
 require 'dm-postgres-adapter'
+require 'sinatra/flash'
 
 class Space
   include DataMapper::Resource
@@ -16,5 +17,17 @@ class Space
   has n, :tags
   has n, :pictures
 
+  def available_dates
+    (self.start_date..self.end_date)
+  end
 
+  def unavailable_dates
+    self.bookings.all(:space_id => self.id,
+                     :status => "Confirmed").map {|booking| booking.date}
+  end
+
+  def is_available_on(date)
+    (self.available_dates.include? date) &&
+    !(self.unavailable_dates.include? date)
+  end
 end
