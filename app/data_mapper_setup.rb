da9638328acae00da2506e2fa0de6f00e3609218
Irene Canuti <irene.canuti@gmail.com>
2017-02-10 10:44:02 +0000

    WIP could not detect rake tasks issue n.2

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,17 +1,17 @@
 require 'data_mapper'
-# require 'database_cleaner'
+require 'database_cleaner'
 require 'dm-postgres-adapter'
-# require 'dm-validations'
-# require 'dm-transactions'
+require 'dm-validations'
+require 'dm-transactions'
 
-# require 'sinatra/base'
-# require 'sinatra/flash'
-#
-# require 'encrypted_cookie'
-# require 'bcrypt'
-# require 'carrierwave/datamapper'
-#
-# require 'pry'
+require 'sinatra/base'
+require 'sinatra/flash'
+
+require 'encrypted_cookie'
+require 'bcrypt'
+require 'carrierwave/datamapper'
+
+require 'pry'
 
 require './app/models/user'
 require './app/models/space'
